48b2d814826700f1fccc90c1d622b6f4ac31520b
Jorge Mora Giménez <jorge@ge.org.es>
2020-04-15 21:23:18 +0200

    no-x509 security config back to default

--- dj-knox-work-no-x509/src/main/java/com/dj/knox/configuration/SecurityConfiguration.java
+++ dj-knox-work-no-x509/src/main/java/com/dj/knox/configuration/SecurityConfiguration.java
@@ -42,22 +42,58 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         if (rememberMeServices instanceof AbstractRememberMeServices) {
             key = ((AbstractRememberMeServices) rememberMeServices).getKey();
         }
-        http.csrf().disable().headers().frameOptions().sameOrigin()
+        http
+            // If we're going to enable it, better to allow token to be pushed through cookies.
+            // That way we could retrieve it using simple HEAD requests to login endpoint.
+            // .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+            .csrf().disable()
 
-                .and().rememberMe().key(key).rememberMeServices(rememberMeServicesObjectProvider.getIfAvailable()).and()
-                .logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()).logoutUrl("/auth/logout")
-                .and().exceptionHandling()
-                .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
-                        AnyRequestMatcher.INSTANCE)
-                .and().formLogin().loginProcessingUrl("/auth/login")
-                .successHandler(new AjaxAuthenticationSuccessHandler())
-                .failureHandler(new AjaxAuthenticationFailureHandler()).and().authorizeRequests()
-                .antMatchers("/analytics-api/**").hasAuthority(SecurityConstants.ACCESS_REPORTS_METRICS)
-                .antMatchers("/template-api/**").hasAuthority(SecurityConstants.ACCESS_TEMPLATE_MANAGEMENT)
-                .antMatchers("/work-object-api/**").hasAuthority(SecurityConstants.ACCESS_WORKOBJECT_API)
-                .antMatchers("/").permitAll()
-                .antMatchers("/**/*.svg", "/**/*.ico", "/**/*.png", "/**/*.woff2", "/**/*.css", "/**/*.woff",
-                        "/**/*.html", "/**/*.js", "/**/index.html")
-                .permitAll().anyRequest().authenticated().and().httpBasic();
+            // Frontend would eventually load browser's PDF viewer using object/embed
+            // To avoid problems in some scenarios, we need to do set X-Frame-Options sameorigin;
+            .headers().frameOptions().sameOrigin()
+
+            // In case you want to disable JSESSIONID cookies.
+            // Flowable's Authentication class expects AuthenticationSuccessEvent to be fired
+            // every time, and sending the cookie makes it fail from time to time. Check class
+            // SecurityAutoConfiguration for insights on how is this configured.
+            // Two things to notice:
+            // 1. You have to add basic auth to /frontend/engage/api/auth.ts
+            //    Axios.defaults.auth = { username, password };
+            // 2. WebSockets+SocksJS, https://github.com/sockjs/sockjs-client/issues/196
+            //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            //.and()
+            .and()
+            .rememberMe()
+            .key(key)
+            .rememberMeServices(rememberMeServicesObjectProvider.getIfAvailable())
+            .and()
+            .logout()
+            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
+            .logoutUrl("/auth/logout")
+            .and()
+            // Non authenticated exception handling. The formLogin and httpBasic configure the exceptionHandling
+            // We have to initialize the exception handling with a default authentication entry point in order to return 401 each time and not have a
+            // forward due to the formLogin or the http basic popup due to the httpBasic
+            .exceptionHandling()
+            .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED), AnyRequestMatcher.INSTANCE)
+            .and()
+            .formLogin()
+            .loginProcessingUrl("/auth/login")
+            .successHandler(new AjaxAuthenticationSuccessHandler())
+            .failureHandler(new AjaxAuthenticationFailureHandler())
+            .and()
+            .authorizeRequests()
+            .antMatchers("/analytics-api/**").hasAuthority(SecurityConstants.ACCESS_REPORTS_METRICS)
+            .antMatchers("/template-api/**").hasAuthority(SecurityConstants.ACCESS_TEMPLATE_MANAGEMENT)
+            .antMatchers("/work-object-api/**").hasAuthority(SecurityConstants.ACCESS_WORKOBJECT_API)
+            // allow context root for all (it triggers the loading of the initial page)
+            .antMatchers("/").permitAll()
+            .antMatchers(
+                    "/**/*.svg", "/**/*.ico", "/**/*.png", "/**/*.woff2", "/**/*.css",
+                    "/**/*.woff", "/**/*.html", "/**/*.js",
+                    "/**/index.html").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
     }
 }
